bd9d57441e99a134cb579900beca61c2e5bb70f5
ThiakiYoshida <thiakiyoshida@gmail.com>
2023-07-18 09:45:29 -0300

    modificacoes na aula 10

--- AulasHTML/Fase04/Aula10-Javascript-Basico/script.js
+++ AulasHTML/Fase04/Aula10-Javascript-Basico/script.js
@@ -1,3 +1,163 @@
-let variavel = "Olá console";
+/*
+let userId = 123456;
+let nomeUsuario = "Eduardo"
 
-console.log(variavel , "-" , typeof variavel);
+console.log("Id:", userId); 
+console.log("Nome:", nomeUsuario);
+
+// Caixar de Diálogo
+
+alert("Alertando")
+confirm("Confirmar alguma coisa")
+prompt("Caixa de inserção de conteúdo")
+*/
+
+
+
+
+
+
+
+
+
+
+// TREINANDO
+
+
+/*
+// Contas
+let num1 = parseFloat(prompt("Digite o primeiro número:"));
+let num2 = parseFloat(prompt("Digite o segundo número:"));
+let result = 0;
+
+// Soma
+result = num1 + num2;
+console.log("A soma de ", num1, "+", num2, "=", result);
+
+// Subtração
+result = num1 - num2;
+console.log("A subtração de ", num1, "-", num2, "=", result);
+
+// Multiplicação
+result = num1 * num2;
+console.log("A multiplicação de ", num1, "*", num2, "=", result);
+
+// Divisão
+result = num1 / num2;
+console.log("A divisão de ", num1, "/", num2, "=", result);
+
+// Módulo
+result = num1 % num2;
+console.log("O resto da divisão de ", num1, "e", num2, "é", result);
+
+// Potência
+result = num1 ** num2;
+console.log("A potência de ", num1, "e", num2, "é", result);
+*/
+
+
+
+
+
+/*
+// RETORNA VERDADEIRO OU FALSO
+
+let num1 = parseFloat(prompt("Digite o primeiro número:"));
+let num2 = parseFloat(prompt("Digite o segundo número:"));
+let result = 0;
+
+// Maior
+result = (num1 > num2);
+console.log("O", num1, "é maior que", num2,"? R:", result);
+
+// Menor
+result = (num1 < num2);
+console.log("O", num1, "é menor que", num2,"? R:", result);
+
+// Maior ou Igual
+result = (num1 >= num2);
+console.log("O", num1, "é maior ou igual que", num2,"? R:", result);
+
+// Menor ou Igual
+result = (num1 <= num2);
+console.log("O", num1, "é menor ou igual que", num2,"? R:", result);
+
+// Igual
+result = (num1 == num2);
+console.log("O", num1, "é igual que", num2,"? R:", result);
+
+// Diferente
+result = (num1 != num2);
+console.log("O", num1, "é diferente que", num2,"? R:", result);
+
+// Valores igual e do mesmo tipo (string, int, float...)
+result = (num1 === num2);
+console.log("O", num1, "é igual e do mesmo tipo que", num2,"? R:", result);
+
+// Valores diferentes e de tipo diferente (string, int, float...)
+result = (num1 !== num2);
+console.log("O", num1, "é diferente e de tipo diferente que", num2,"? R:", result);
+*/
+
+
+
+
+
+/*
+// Facilitando as contas
+
+let valor = 100;
+
+// Soma mais simples
+valor += 10
+console.log("A soma de 100 e 10 ficou =", valor)
+
+// Subtração mais simples
+valor -= 10
+console.log("A subtração de 100 e 10 ficou =", valor)
+
+// Multiplicação mais simples
+valor *= 10
+console.log("A multiplicação de 100 e 10 ficou =", valor)
+
+// Divisão mais simples
+valor /= 10
+console.log("A divisão de 100 e 10 ficou =", valor)
+*/
+
+
+
+
+
+/*
+//FUNÇÕES MATH
+
+let valor = 10.11
+
+// Retorna o próximo número inteiro 
+console.log("Math.ceil", Math.ceil(valor))
+
+// Retorna o anterior número inteiro 
+console.log("Math.floor", Math.floor(valor))
+
+// Retorna o número aproximado 
+console.log("Math.round", Math.round(valor))
+
+// Retorna o maior número 
+console.log("Math.max", Math.max(10, 20, 30, 40, 50))
+
+// Retorna o menor número 
+console.log("Math.min", Math.min(10, 20, 30, 40, 50))
+
+// Retorna a potência número (2 elevado a 5)
+console.log("Math.pow", Math.pow(2, 5))
+
+// Retorna um valor aleatório entre 0 e 1 
+console.log("Math.random", Math.random())
+
+// Retorna a raiz quadrada 
+console.log("Math.sqrt", Math.sqrt(valor))
+
+// Retorna a raiz cúbica 
+console.log("Math.cbrt", Math.cbrt(valor))
+*/
