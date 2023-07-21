4688669e14f5a5a2a1ba45ef2934f0a85f144271
ThiakiYoshida <thiakiyoshida@gmail.com>
2023-07-21 09:40:38 -0300

    enviando aula 11

---
+++ AulasHTML/Fase04/Aula11-Javascript-DOM/js/script.js
@@ -0,0 +1,50 @@
+// FUNCTION
+/*
+Sintaxe
+    function {nome-funcao}(parâmetros) {
+        // Script da função
+    }
+
+
+
+// ------------------------------- //
+
+
+// FUNCTION EXPRESS
+// Serve para condensar funções
+Sintaxe
+    const {nome-funcao} = function(parâmetros) {
+       // Script da função 
+    }
+
+
+
+// ------------------------------- //
+
+
+
+// ARROW FUNCTION
+// Serve para condensar funções
+Sintaxe
+    const {nome-funcao} = (parâmetros) => {
+       // Script da função 
+    }
+
+*/
+
+//TESTANDO
+
+// TABUADA COM FUNÇÃO
+/*
+function tabuada (numeroTab){
+    console.log(`Estamos fazendo a tabuada do número ${numeroTab}`)
+    for (let i =0 ; i <= 10 ; i++){
+        let soma = numeroTab * i
+        console.log(`${numeroTab} x ${i} = ${soma}`)
+    }
+    console.log("<-----Final da tabuada----->")
+}
+
+let numTabuada = prompt("Digite o numero da tabuada")
+tabuada (numTabuada)
+*/
