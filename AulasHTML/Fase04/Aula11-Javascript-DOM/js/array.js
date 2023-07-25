b24db6b37f7c5a6971a496d9fec53dcf775de568
unknown <thiakiyoshida@gmail.com>
2023-07-24 22:56:41 -0300

    atualização da aula 11

--- AulasHTML/Fase04/Aula11-Javascript-DOM/js/array.js
+++ AulasHTML/Fase04/Aula11-Javascript-DOM/js/array.js
@@ -1,3 +1,4 @@
+/*
 const arrayExemplo = ['Aluno', 'Planta', 'Diretor', 'Coordenador']
 
 // MODIFICANDO O SEGUNDO ELEMENTO DA ARRAY
@@ -22,5 +23,42 @@ arrayExemplo.pop()
 // Primeiro valor é o qual vai começar esse argumento / Segundo é qual índice será apagado / Terceiro é qual elemento será adicionado no ídice que foi demaracado no primeiro valor
 arrayExemplo.splice(1, 0, 'Teste')
 
+// MOSTRA O QUE DEVE SER EXIBIDO, O PRIMEIRO VALOR É O ÍNDICE DE O SEGUNDO O ÍNDICE DE FIM 
+arrayExemplo.slice(0, 1)
 
-console.log(arrayExemplo)
+// MOSTRA O ÍNDICE DO PRIMEIRO VALOR ENCONTRADO
+arrayExemplo.indexOf('Aluno')
+
+// MOSTRA O ÍNDICE DO ÚLTIMO VALOR ENCONTRADO
+arrayExemplo.lastIndexOf('Aluno')
+
+// JUNTA DOIS ARRAYS, CRIANDO UM NOVO ARRAY
+array01 = ['a', 'b', 'c']
+arrayConcat = arrayExemplo.concat(array01)
+
+// COLOCA OS ELEMENTOS EM ORDEM ALFABÉTICA
+arrayExemplo.sort()
+
+// INVERTE OS ELEMENTOS DA ARRAY
+arrayExemplo.reverse()
+
+// TRANSFORMA A ARRAY EM UMA STRING E É POSSÍVEL COLOCAR UM SEPARADOR ENTRE ELES
+arrayExemplo.join('-')
+
+// Retorna com true ou false se o elemento está presente na array
+arrayExemplo.includes('Teste')
+
+
+console.table(arrayExemplo)
+
+*/
+// --------------------------------------- //
+
+arrayManipulacao = ['Notebook', 'Smarphone', 'Smartwatch', 'Tablet', 'Playstation 5']
+
+// EVERY() TEM COMO OBJETIVO MOSTRAR PARA GENTE SE TODOS OS ELEMENTOS DA ARRAY TEM UMA DETERMINADA CONDIÇÃO
+
+
+
+
+console.table(arrayManipulacao)
--- AulasHTML/Fase04/Aula11-Javascript-DOM/js/funcao.js
+++ AulasHTML/Fase04/Aula11-Javascript-DOM/js/funcao.js
@@ -53,6 +53,8 @@ let numTabuada = prompt("Digite o numero da tabuada")
 tabuada (numTabuada)
 
 
+
+
 // FUNCÇÃO CALLBACK
 function somar(x, y, z){
     return (x + y + z)
@@ -73,3 +75,24 @@ function calcular(x, y, z, conta){
 console.log(`A soma é ${calcular(10, 20, 20, somar)}`)
 console.log(`A subtração é ${calcular(10, 20, 20, subtrair)}`)
 console.log(`A media é ${calcular(10, 20, 20, media)}`)
+
+
+
+
+
+
+// SENHA ALEATÓRIA DE 20 DIGITOS
+const gerarSenha = () => {
+    // Declarando os números da senha
+    const caracteres = 'ABCDEFGHIJKLMNOPQRSUVWXYZabcdefghijklmnopqrsuvwxyz1234567890!@#$%&amp;*';
+    let novaSenha = '';
+    // Repetindo 20 vezes a escolha de caracteres
+    for (let i = 0; i <= 19; i++) {
+        // Colocando na variável indice a escolha aleatória pelo Math.random e depois multiplica pelos caracteres.length que calcula a quantidade de caracteres da variável, fazendo com que tenha o numero do caracter que será escolhido
+        const indice = Math.round(Math.random() * caracteres.length);
+        // Coloca na varivel novaSenha, a própria variável e acrescenta o caracter que esta dentro da variável caracter de indice da variável indice que foi caculado
+        novaSenha += caracteres[indice];
+    }
+    return novaSenha;
+}
+console.log(`A senha gerada foi: ${gerarSenha()}`);
