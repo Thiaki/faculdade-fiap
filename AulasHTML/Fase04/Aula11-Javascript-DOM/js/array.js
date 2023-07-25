32fe741e84cadab4e4dedf8dceed121682ae0896
ThiakiYoshida <thiakiyoshida@gmail.com>
2023-07-25 09:47:01 -0300

    atualizações aula 11 javascript

--- AulasHTML/Fase04/Aula11-Javascript-DOM/js/array.js
+++ AulasHTML/Fase04/Aula11-Javascript-DOM/js/array.js
@@ -57,8 +57,35 @@ console.table(arrayExemplo)
 arrayManipulacao = ['Notebook', 'Smarphone', 'Smartwatch', 'Tablet', 'Playstation 5']
 
 // EVERY() TEM COMO OBJETIVO MOSTRAR PARA GENTE SE TODOS OS ELEMENTOS DA ARRAY TEM UMA DETERMINADA CONDIÇÃO
+// Colocando na variável possuiLetraA o retorno true ou false
+let possuiLetraA = arrayManipulacao.every(
+    // Criando uma função para verificar se todos os elementos possuem a letra A
+    // produto seria onde cada elemento do array passa, default o every verifica cada elemento da array
+    produto => {
+    return produto.includes('a');
+}
+)
 
 
 
+// SOME() VERIFICA SE PELO MENOS 1 ELEMENTO DA ARRAY TEM UMA DETERMINADA CONDIÇÃO
+// Colocando na variável maxCaracteres10 o retorno true ou false
+const maxCaracteres10 = arrayManipulacao.some(
+    // Criando uma função para verificar se dentro da array tem algum elemento mais que 10 caracteres
+    // produto seria onde cada elemento do array passa, default o some verifica cada elemento da array
+    produto => {
+        return produto.length <= 10;
+    }
+)
 
-console.table(arrayManipulacao)
+
+
+// FIND() RETORNA O PRIMEIRO ELEMENTO QUE ESTA DENTRO DA CONDIÇÃO DETERMINADA
+const possuiLetraO = arrayManipulacao.find(
+    produto => {
+        return produto.includes('o')
+    }
+) 
+
+
+console.log(arrayManipulacao)
