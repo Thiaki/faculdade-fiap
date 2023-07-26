57a41ff9bb3f27e98954dc878c3a7a1a88b4d389
unknown <thiakiyoshida@gmail.com>
2023-07-25 22:45:04 -0300

    atualizando aula 11

--- AulasHTML/Fase04/Aula11-Javascript-DOM/js/array.js
+++ AulasHTML/Fase04/Aula11-Javascript-DOM/js/array.js
@@ -51,10 +51,11 @@ arrayExemplo.includes('Teste')
 
 console.table(arrayExemplo)
 
-*/
+
 // --------------------------------------- //
 
-arrayManipulacao = ['Notebook', 'Smarphone', 'Smartwatch', 'Tablet', 'Playstation 5']
+
+arrayManipulacao = ['Notebook', 'Smartphone', 'Smartwatch', 'Tablet', 'Playstation 5']
 
 // EVERY() TEM COMO OBJETIVO MOSTRAR PARA GENTE SE TODOS OS ELEMENTOS DA ARRAY TEM UMA DETERMINADA CONDIÇÃO
 // Colocando na variável possuiLetraA o retorno true ou false
@@ -88,4 +89,95 @@ const possuiLetraO = arrayManipulacao.find(
 ) 
 
 
-console.log(arrayManipulacao)
+// --------------------------------------- //
+
+
+arrayString = ['Notebook', 'Smartphone', 'Smartwatch', 'Tablet', 'Playstation 5']
+arrayNumber = [100, 200, 300, 500, 600, 700, 800, 1000, 1100, 1300]
+
+// FILTER() RETORNA UM NOVO ARRAY COM OS VALORES QUE SATISFAZEM A CONDIÇÃO DEFINIDA
+// Esta colocando dentro da variável palavra todos os produtos que possuem Smart
+const palavra = arrayString.filter(produto => {
+    return produto.includes('Smart')
+})
+console.log(palavra)
+
+const maiorValor = arrayNumber.filter(valor => {
+    return valor > 500
+})
+console.log(maiorValor)
+
+
+
+// MAP() APLICA UMA FUNÇÃO EM CADA ELEMENTO DO ARRAY E RETORNA UM NOVO PARA TODO ELEMENTO QUE SATISFAÇA
+// Colocando dentro da variável letraMaiuscula todos os elementos em maiúsculo
+const letraMaiuscula = arrayString.map(produto => {
+    // Colocando todas as strings em maiúsculo
+    return produto.toUpperCase()
+})
+console.log(letraMaiuscula)
+
+// Coloca na variável desconto todos os elementos com um desconto de 10%
+const desconto = arrayNumber.map(valor => {
+    // Coloca todas os numbers -10%
+    return valor - (valor * 0.1)
+})
+console.log(desconto)
+
+
+
+// REDUCE() APLICA UMA FUNÇÃO EM CADA ELEMENTO DO ARRAY E RETORNA APENAS UM VALOR
+// Coloca na variável totalValores a soma de todos os valores da array.
+// Define o "acumulador" que vai juntar os valores e o "valor" que é cada elemento da array
+const totalValores = arrayNumber.reduce((acumulador, valor) => {
+    // Retorna o "acumulador" somado a todos os elementos.
+    return acumulador + valor
+// Definindo o "acumulador" em 0
+}, 0)
+console.log(totalValores)
+
+*/
+// --------------------------------------- //
+
+
+const arrayProdutos = ['Smartphone', 'Notebook', 'Smartwatch', 'Tablet', 'Teclado', 'Mouse', 'Caixa de som', 'Fone de ouvido', 'Webcam', 'Roteador' ]
+
+// UTILIZANDO O FOR PARA VERIFICAR NOSSOS ELEMENTOS EDNTRO DO ARRAY
+// Definindo o contador (i) e o array lenght para verifica a quantidade de vezes que o for será feito
+for(let i = 0; i < (arrayProdutos.length); i++) {
+    // Exibindo o elemento i (ex: primeiro 1 , segundo 2 ...) do array arrayProdutos
+    console.log(`Indice ${i} do array é: ${arrayProdutos[i]}`)
+}
+
+console.log("<------------------------------>")
+
+// UTILIZANDO O FOR IN PARA VISUALIZAR TODOS OS ELEMENTOS
+// Com o for in ele automaticamente ve quantos elemento o array possui e roda ele inteiro, sem precisar colocar o i++ e nem a contagem dos elementos do array
+for (let i in arrayProdutos) {
+    console.log(`Indice ${i} do array é: ${arrayProdutos[i]}`)
+}
+
+console.log("<------------------------------>")
+
+// UTILIZANDO O FOR OFF PARA DEIXAR OS ELEMENTOS DO ARRAY EM MAIÚSCULO
+// O for off despreza o indice e checa os elementos do array
+for (let elemento of arrayProdutos) {
+    console.log(elemento.toUpperCase())
+}
+
+console.log("<------------------------------>")
+
+// FOT EACH APLICA UMA FUNÇÃO PARA CADA INDICE DOS ELEMENTOS DE UMA ARRAY
+let maiorCaracter = 0
+
+// Depois do for each esta colocando uma funcao com o parametro produto que vai guardar todos os elementos do array um de cada vez, fazendo com que passe pela função um array de cada vez 
+arrayProdutos.forEach( produto => {
+    console.log(`${produto} possui ${produto.length} caracteres`)
+    // Fazendo um if para descobrir qual o produto com mais caracteres
+    if (produto.length > maiorCaracter){
+        maiorCaracter = produto.length
+        maiorProduto = produto
+    }
+})
+
+console.log(`O ${maiorProduto} possui mais caracteres, com ${maiorCaracter} caracteres`)
