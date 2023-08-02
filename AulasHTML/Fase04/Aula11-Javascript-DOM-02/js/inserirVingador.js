3d029667492ae856e7313e214f4239f95687bb5e
ThiakiYoshida <thiakiyoshida@gmail.com>
2023-08-02 09:41:16 -0300

    finaliznado aula 11 javascript

--- AulasHTML/Fase04/Aula11-Javascript-DOM-02/js/inserirVingador.js
+++ AulasHTML/Fase04/Aula11-Javascript-DOM-02/js/inserirVingador.js
@@ -1,56 +1,49 @@
-// const forms = document.querySelector('form')
-// forms.addEventListener('submit', function(eventoSubmit) {
-//     // Cancelar o evento submit pois não esta enviando para nenhum lugar esse formulário
-//     eventoSubmit.preventDefault()
-//     const formVingadores = forms.querySelector('input')
-//     const novoVingador = {};
-//     formVingadores.forEach(campo => {
-//         novoVingador[campo.name] = campo.value
-//     })
-//     novoVingador.xp = calcularXP(novoVingador.forca , novoVingador.agilidade , novoVingador.velocidade).toFixed(1)
-    
-//     const tr = document.createElement('tr')
-//     tr.classList.add('bg-secondary', "text-light")
-    
-//     for(let dado in novoVingador) {
-//         const td = document.createElement(td)
-        
-//         td.textContent = novoVingador[dado]
-//         tr.appendChild(td)
-//     }
-//     const tbody = document.querySelector('#vingadores')
-//     tbody.appendChild(tr)
+const forms = document.querySelector('form')
+// Colocando no form um escutador de quando o formulário for enviado
+forms.addEventListener('submit', function(eventoSubmit) {
+    // Cancelar o evento submit pois não esta enviando para nenhum lugar esse formulário
+    eventoSubmit.preventDefault()
 
-//     this.reset()
+    // Procurando os campos de preenchimento do usuário
+    const formVingadores = forms.querySelectorAll('input')
 
-// })
+    // Atribuindo o novoVingador como uma array
+    const novoVingador = [];
 
 
+    formVingadores.forEach(campo => {
+        // formVingadores seria todos os campos onde o usuário vai colocar sobre o herói novo
+        // campo seria o nome de cada um, já que esta dentro de um forEach ele vai passar um de cada vez
+        // campo.name é o nome de cada campo que ele vai passar
+        // campo.value é o digitado pelo usuário
 
-
-const tbody = document.querySelector('tbody')
-
-document.querySelector('form').addEventListener('submit', function(eventoSubmit) {
-    eventoSubmit.preventDefault();
-
-    const novoVingador = [
-        document.querySelector('#nome'),
-        document.querySelector('#forca'),
-        document.querySelector('#velocidade'),
-        document.querySelector('#agilidade'),
-    ]
-
-    const tr = document.createElement('tr')
-    tr.classList.add('vingador', 'bg-secondary', "text-light")
-
-    // Para cada campo que criamos (nome, forca, agilidade, velocidade) é criado uma td diferente e todas ficam dentro de um mesmo tr que foi criado anteriormente
-    novoVingador.forEach(campo => {
-        const td = document.createElement('td')
-        td.textContent = campo.value
-        tr.appendChild(td)
+        // ESTA ATRIBUINDO O DIGITADO PELO USUÁRIO PARA O ARRAY NOVO VINGADOR COM OS NOMES DOS CAMPOS E OS VALORES DIGITADOS
+        novoVingador[campo.name] = campo.value
     })
+    novoVingador.xp = calcularXP(novoVingador.forca , novoVingador.agilidade , novoVingador.velocidade).toFixed(1)
+    
+    // Criando os elementos html
+    const tr = document.createElement('tr')
+    // Colocando as classes
+    tr.classList.add('bg-success', "text-light")
+    
 
+    // Dado é o campo.name
+    // in - se o dado estiver em novoVingador, ele entra no for
+    for(let dado in novoVingador) {
+        // Criando o elemento html
+        const td = document.createElement('td')
+        
+        // Atribuindo o dado do array para o texto desse html
+        td.textContent = novoVingador[dado]
+        // Colocando o td dentro do tr
+        tr.appendChild(td)
+    }
+
+    const tbody = document.querySelector('#vingadores')
     tbody.appendChild(tr)
 
+    // Apagando o formulário
     this.reset()
+
 })
